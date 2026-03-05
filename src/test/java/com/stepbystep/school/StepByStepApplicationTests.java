461efd1da8badd6a97acd5a22d760cc7a83d6d52
andreyferraz <andrey.developer@hotmail.com>
2026-03-05 03:10:08 -0300

    first commit

---
+++ src/test/java/com/stepbystep/school/StepByStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepbystep.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepByStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
