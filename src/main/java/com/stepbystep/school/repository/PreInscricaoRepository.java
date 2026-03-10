9d06f294eb9b20c8eb3a61cb67ef369f7deef19d
andreyferraz <andrey.developer@hotmail.com>
2026-03-10 09:25:35 -0300

    add findAllByOrderByDataLeadDesc method to PreInscricaoRepository for ordering pre-registrations by lead date

--- src/main/java/com/stepbystep/school/repository/PreInscricaoRepository.java
+++ src/main/java/com/stepbystep/school/repository/PreInscricaoRepository.java
@@ -8,4 +8,6 @@ import com.stepbystep.school.model.PreInscricao;
 
 public interface PreInscricaoRepository extends JpaRepository<PreInscricao, UUID> {
 
+	java.util.List<PreInscricao> findAllByOrderByDataLeadDesc();
+
 }
